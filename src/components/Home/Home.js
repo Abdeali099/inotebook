17aefa2b7b3e75983a0e679d9d4f1a33f45d63ba
Abdeali <abdealichharchhoda786@gmail.com>
2022-12-06 12:30:02 +0530

    Made Basic structure of fetched notes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import NoteContext from '../../context/notes/NoteContext'
+import { Link} from 'react-router-dom'
+
 
 function Home() {
+
+  const context = useContext(NoteContext);
+
+  const { savedNotes, setSavedNotes } = context;
+
   return (
 
     <>
@@ -21,7 +29,7 @@ function Home() {
 
           <div className="mb-3">
             <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" />
+            <input type="password" className="form-control" id="exampleInputPassword1" autoComplete='true' />
           </div>
 
           <div className="mb-3 form-check">
@@ -37,6 +45,25 @@ function Home() {
 
       <div className="container my-3">
         <h2>Your Notes</h2>
+
+        <div className="container my-3 d-flex justify-between">
+
+          {savedNotes.map((Element => {
+
+            return <div className="card" style={{width: "18rem",margin:"1rem"}}>
+              <div className="card-body">
+                <h5 className="card-title">{Element.title}</h5>
+                <p className="card-text">{Element.content}</p>
+                <p className="card-text">{Element.tag}</p>
+                <Link to="/" className="btn btn-primary">Go somewhere</Link>
+              </div>
+            </div>
+
+          }))}
+
+
+        </div>
+
       </div>
 
     </>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React ,{ useState }from 'react'
 import NoteContext from "./NoteContext";
+
 
 const NoteState = (props) => {
 
-    /* const state = {
-        Name: "Abdeali",
-        Class: "CSE"
-    }
- */
-    /* console.log("Props Children : ");
-    console.log(props);
-    console.log(props.children); */
+    const fetchedNotes = [
+        {
+            "_id": "63885557f4a9c272a5a763d8",
+            "user": "638722221121dc325c4a5a7f",
+            "title": "First Notes",
+            "content": "I am very Happy today",
+            "tag": "happy",
+            "date": "2022-12-01T07:18:47.832Z",
+            "__v": 0
+        },
+        {
+            "_id": "638ee24514fbcd4a5f7bb695",
+            "user": "638722221121dc325c4a5a7f",
+            "title": "Second Notes",
+            "content": "I am very Happy right Now",
+            "tag": "test",
+            "date": "2022-12-06T06:33:41.146Z",
+            "__v": 0
+        }
+    ];
+
+    const [savedNotes, setSavedNotes] = useState(fetchedNotes);
 
     return (
 
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{savedNotes,setSavedNotes}}>
             {props.children}
         </NoteContext.Provider>
 
