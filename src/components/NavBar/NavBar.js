9807423fd28ccc14a9a38be49eb73cc58610ae6d
Abdeali <abdealichharchhoda786@gmail.com>
2022-12-30 22:50:59 +0530

    Made Changes In NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,13 @@
-import React ,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
 function Navbar() {
 
     let Location = useLocation();
-    
-/*     useEffect(() => {
-        console.log(Location.pathname);
-    }, [Location]) */ // (No Need)
+
+    /*     useEffect(() => {
+            console.log(Location.pathname);
+        }, [Location]) */ // (No Need)
 
 
     return (
@@ -18,24 +18,29 @@ function Navbar() {
 
                     <Link className="navbar-brand" to="/">iNoteBook</Link>
 
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
-                        <ul className="navbar-nav">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
                             <li className="nav-item">
-                                <Link className={`nav-link ${( Location.pathname===("/home") || (Location.pathname==="/") ) ? "active" : ""}`}  to="/home">Home</Link>
+                                <Link className={`nav-link ${(Location.pathname === ("/home") || (Location.pathname === "/")) ? "active" : ""}`} to="/home">Home</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link className={`nav-link ${Location.pathname==="/about" ? "active" : ""}`} to="/about">About</Link>
+                                <Link className={`nav-link ${Location.pathname === "/about" ? "active" : ""}`} to="/about">About</Link>
                             </li>
 
-                            
                         </ul>
+
+                        <form className="d-flex" style={{alignItems: "center",justifyContent: "center",gap: "1rem"}}>
+                            <Link to="/" className="btn btn-primary mx-2 me-2" role="button">SignIn</Link>
+                            <Link to="/" className="btn btn-primary max-2 me-2" role="button">SignUp</Link>
+                        </form>
+
 
                     </div>
 
